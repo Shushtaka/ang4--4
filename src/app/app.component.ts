963849bb86a1d70b2c4c5d82736ad6655327d192
LanaNew <l4n.steen@gmail.com>
2025-03-09 16:51:17 +0400

    version 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, FormsModule],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, FormsModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
